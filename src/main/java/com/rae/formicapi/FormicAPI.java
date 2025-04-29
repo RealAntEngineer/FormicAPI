9336b8d6dc2d841bd4b2ccb481dc7134508e25ab
Real Ant Engineer <lafourmiedu35@gmail.com>
2025-04-29 21:46:47 +0200

    Initial commit

---
+++ src/main/java/com/rae/formicapi/FormicAPI.java
@@ -0,0 +1,11 @@
+package com.rae.formicapi;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(FormicAPI.MODID)
+public class FormicAPI {
+    public static final String MODID = "formicapi";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+}
