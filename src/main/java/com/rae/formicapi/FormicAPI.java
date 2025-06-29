686de4ab02fbe54359d1a0ceea4ad31e564e3be3
Real Ant Engineer <lafourmiedu35@gmail.com>
2025-06-29 11:44:20 +0200

    try to solve weird desobf issue

--- src/main/java/com/rae/formicapi/FormicAPI.java
+++ src/main/java/com/rae/formicapi/FormicAPI.java
@@ -1,6 +1,11 @@
 package com.rae.formicapi;
 
+import com.rae.formicapi.config.FormicAPIConfigs;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,4 +13,11 @@ import org.apache.logging.log4j.Logger;
 public class FormicAPI {
     public static final String MODID = "formicapi";
     public static final Logger LOGGER = LogManager.getLogger(MODID);
+    public FormicAPI() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
+        ModLoadingContext modLoadingContext = ModLoadingContext.get();
+        FormicAPIConfigs.registerConfigs(modLoadingContext);
+
+    }
 }
